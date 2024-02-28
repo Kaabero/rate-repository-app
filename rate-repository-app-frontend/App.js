1fd990143814d63bdb42b573fb3f9949e0dabca5
Katri <katri.kuoppala@helsinki.fi>
2024-02-28 12:28:21 +0200

    updating to updated material

--- rate-repository-app-frontend/App.js
+++ rate-repository-app-frontend/App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('debuggaus')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your application!!</Text>
+      <Text>Open up App.js to start working on your application!</Text>
       <StatusBar style="auto" />
     </View>
   );
