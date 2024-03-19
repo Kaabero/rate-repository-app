5e6f2ee6befac0b8a490978bf5a71dd73b98e4cf
Katri <katri.kuoppala@helsinki.fi>
2024-03-19 11:55:23 +0200

    Exercise 10.20: repository's review list fixies

--- rate-repository-app-frontend/src/components/SingleRepository.jsx
+++ rate-repository-app-frontend/src/components/SingleRepository.jsx
@@ -2,9 +2,17 @@ import { useParams } from 'react-router-native';
 import useRepositories from '../hooks/useRepositories';
 import RepositoryItem from './RepositoryItem';
 import { useState } from 'react'
-import { View } from 'react-native';
-import Reviews from './Reviews';
-import { FlatList } from 'react-native';
+import { FlatList, View, StyleSheet } from 'react-native';
+import ReviewItem from './ReviewItem';
+
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 
 const SingleRepository = () => {
@@ -24,24 +32,32 @@ const SingleRepository = () => {
   : [];
   
   return ( 
-    <View>
-    <RepositoryItem 
-      name={repository.fullName} 
-      description={repository.description} 
-      language={repository.language} 
-      stars={repository.stargazersCount} 
-      forks={repository.forksCount} 
-      reviews={repository.reviewCount} 
-      rating={repository.ratingAverage}
-      image={repository.ownerAvatarUrl}
-      singleRepository={single}
-      url={repository.url}
-    />
-    <Reviews reviews={reviewNodes}/>
-    </View>
-    
-    
-    
+    <FlatList
+    data={reviewNodes}
+    ItemSeparatorComponent={ItemSeparator}
+    renderItem={({item}) => 
+      <ReviewItem 
+        rating={item.rating} 
+        created={item.createdAt} 
+        text={item.text} 
+        user={item.user}
+      />
+    }
+    keyExtractor={item => item.id}
+    ListHeaderComponent={() =>
+      <RepositoryItem 
+        name={repository.fullName} 
+        description={repository.description} 
+        language={repository.language} 
+        stars={repository.stargazersCount} 
+        forks={repository.forksCount} 
+        reviews={repository.reviewCount} 
+        rating={repository.ratingAverage}
+        image={repository.ownerAvatarUrl}
+        singleRepository={single}
+        url={repository.url}
+      />}
+    />  
   );
 };
 
